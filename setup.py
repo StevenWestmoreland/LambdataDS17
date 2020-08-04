b4d5fb5090f5c3ef07bf38aeeb766069f16c5c02
StevenWestmoreland <stevenmwestmoreland@gmail.com>
2020-08-04 15:45:33 -0400

    Removed unnecessary import

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", 'r') as file:
 
 setuptools.setup(
     name='lambdata-SWDS17',
-    version='0.0.5',
+    version='0.0.6',
     author='StevenWestmoreland',
     description='A collection of Data Science helper functions',
     long_description=LONG_DESCRIPTION,
